67731b946874251be238905692422f947f8b0fb7
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-05-20 13:59:34 +0200

    feat: fulfill

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const chain = require('chain-sdk')
 const moment = require('moment')
+const crypto = require('crypto')
 
 const client = new chain.Client()
 const signer = new chain.HsmSigner()
@@ -27,6 +28,7 @@ const ESCROW_CONTRACT_SOURCE =
   }
 }`
 
+// createLockingTx and createUnlockingTx taken from https://github.com/chain/chain/blob/ivy/ivy/playground/core/index.tsx
 const createLockingTx = (actions) => {
   return client.transactions.build(builder => {
     actions.forEach(action => {
@@ -58,6 +60,86 @@ const createLockingTx = (actions) => {
     return utxos.items.find(utxo => utxo.purpose !== 'change')
   })
 }
+
+const createUnlockingTx = (actions,
+  witness,
+  mintimes,
+  maxtimes
+) => {
+    return client.transactions.build(builder => {
+      actions.forEach(action => {
+        switch (action.type) {
+          case "spendFromAccount":
+            builder.spendFromAccount(action)
+            break
+          case "controlWithReceiver":
+            builder.controlWithReceiver(action)
+            break
+          case "controlWithAccount":
+            builder.controlWithAccount(action)
+            break
+          case "spendUnspentOutput":
+            builder.spendAnyUnspentOutput(action)
+            break
+          default:
+            break
+        }
+      })
+
+      if (mintimes.length > 0) {
+        const findMax = (currMax, currVal) => {
+          if (currVal.getTime() > currMax.getTime()) {
+            return currVal
+          }
+          return currMax
+        }
+        const mintime = new Date(mintimes.reduce(findMax, mintimes[0]))
+        builder.minTime = new Date(mintime.setSeconds(mintime.getSeconds() + 1))
+      }
+
+      if (maxtimes.length > 0) {
+        const findMin = (currMin, currVal) => {
+          if (currVal.getTime() < currMin.getTime()) {
+            return currVal
+          }
+          return currMin
+        }
+        const maxtime = maxtimes.reduce(findMin, maxtimes[0])
+        builder.maxTime = new Date(maxtime.setSeconds(maxtime.getSeconds() - 1))
+      }
+    }).then((tpl) => {
+      tpl.includesContract = true
+      // TODO(boymanjor): Can we depend on contract being on first utxo?
+      tpl.signingInstructions[0].witnessComponents = witness
+      tpl.signingInstructions.forEach((instruction, idx) => {
+        instruction.witnessComponents.forEach((component) => {
+          if (component.keys === undefined) {
+            return
+          }
+          component.keys.forEach((key) => {
+            signer.addKey(key.xpub, client.mockHsm.signerConnection)
+          })
+        })
+      })
+      return signer.sign(tpl)
+    }).then((tpl) => {
+      witness = tpl.signingInstructions[0].witnessComponents
+      if (witness !== undefined) {
+        tpl.signingInstructions[0].witnessComponents = witness.map(component => {
+          switch(component.type) {
+            case "raw_tx_signature":
+              return {
+                type: "data",
+                value: component.signatures[0]
+              }
+            default:
+              return component
+          }
+        })
+      }
+      return client.transactions.submit(tpl)
+    })
+  }
 
 async function createEscrow ({
   sourceAccountId,
@@ -108,18 +190,100 @@ async function createEscrow ({
   return utxo
 }
 
-createEscrow({
-  sourceAccountId: 'acc0WT9HZ9M00808',
-  sourceProgram: '766baa20cbef1bd2822daa5e7ecc6f6c166eab613afa7328da43d10952541431f21dc4b45151ad696c00c0',
-  sourceKey: 'abcd',
-  destinationAccountId: 'acc0WT9HZ9HG0806',
-  destinationProgram: '766baa209d4a124b801f32e631a910ff4983a4d069a76dd3e1f946e50cf0f5bdc020b19a5151ad696c00c0',
-  destinationKey: 'abcd',
-  amount: 13,
-  assetId: '3d7e4af97c9635c048f72ee943e6bc2b9fcac763bf0f7d4035a076cfc40319ca',
-  expiresAt: moment().add(1, 'days'),
-  condition: '5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03'
-})
-  .then(escrowedTx => console.log(escrowedTx))
-  .catch(err => console.log(err))
+async function fulfill ({
+  fulfillment,
+  escrowUtxo,
+  expiresAt,
+  destinationProgram
+}) {
+  const actions = [{
+    type: 'spendUnspentOutput',
+    outputId: escrowUtxo.id
+  }, {
+    type: 'controlWithReceiver',
+    amount: escrowUtxo.amount,
+    assetId: escrowUtxo.assetId,
+    receiver: {
+      expiresAt: expiresAt.toISOString(),
+      controlProgram: destinationProgram
+    }
+  }]
+
+  const witness = [{
+    type: 'data',
+    value: fulfillment
+  }, {
+    // don't know what this one is about
+    type: 'data',
+    value: '0000000000000000'
+  }]
+
+  const maxtimes = [
+    expiresAt.toDate()
+  ]
+  const mintimes = []
+
+  const tx = await createUnlockingTx(
+    actions,
+    witness,
+    mintimes,
+    maxtimes
+  )
+  return tx
+}
+
+function hash (preimage) {
+  const h = crypto.createHash('sha256')
+  h.update(Buffer.from(preimage, 'hex'))
+  return h.digest()
+}
+
+const sourceAccountId = 'acc0WT9HZ9M00808'
+const sourceKey = 'abcd'
+const destinationAccountId = 'acc0WT9HZ9HG0806'
+const destinationKey = 'abcd'
+const amount = 19
+const assetId = '3d7e4af97c9635c048f72ee943e6bc2b9fcac763bf0f7d4035a076cfc40319ca'
+const expiresAt = moment().add(1, 'days')
+const fulfillment = crypto.randomBytes(32).toString('hex')
+const condition = hash(fulfillment).toString('hex')
+
+
+async function runTest () {
+  const sourceReceiver = await client.accounts.createReceiver({
+    accountId: sourceAccountId
+  })
+  const sourceProgram = sourceReceiver.controlProgram
+
+  const destinationReceiver = await client.accounts.createReceiver({
+    accountId: destinationAccountId
+  })
+  const destinationProgram = destinationReceiver.controlProgram
+
+  const escrowUtxo = await createEscrow({
+    sourceAccountId,
+    sourceProgram,
+    sourceKey,
+    destinationAccountId,
+    destinationProgram,
+    destinationKey,
+    amount,
+    assetId,
+    expiresAt,
+    condition
+  })
+
+  console.log('created escrow utxo: ', escrowUtxo)
+
+  const fulfillTx = await fulfill({
+    fulfillment,
+    destinationProgram,
+    expiresAt,
+    escrowUtxo
+  })
+
+  console.log('fulfilled escrow with tx: ', fulfillTx)
+}
+
+runTest().catch(err => console.log(JSON.stringify(err)))
 
